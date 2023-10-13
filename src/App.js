ee32a204d9fb46cb166554ea591214b515b3e88f
Andrii Ratsilevych <andrii.racilewicz@gmail.com>
2023-10-13 13:04:59 +0200

    add minimax

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import { useState } from 'react';
 
@@ -10,27 +9,98 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-function Board({ xIsNext, squares, onPlay }) {
+function Board({ squares, onPlay }) {
+  cells = Array.from(Array(9).keys())
+
   function handleClick(i) {
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
+    // if (calculateWinner(squares) || squares[i]) {
+    //   return;
+    // }
+
     const nextSquares = squares.slice();
-    if (xIsNext) {
-      nextSquares[i] = 'X';
-    } else {
-      nextSquares[i] = 'O';
-    }
+    nextSquares[i] = 'X';
+    // nextSquares[i] = 'O';
     onPlay(nextSquares);
   }
 
+  function bestSpot(squares) {
+    let availSpots = emptySquares();
+
+	// если при текущем расположении побеждает игрок
+  const winner = calculateWinner(squares);
+	if (winner(newBoard, huPlayer)) {
+		// отнимаем от результата 10 очков
+		return {score: -10};
+	// если выиграет компьютер
+	} else if (winner(newBoard, aiPlayer)) {
+		// прибавляем 10 очков
+		return {score: 10};
+	// если ничья, то не менеяем очки
+	} else if (availSpots.length === 0) {
+		return {score: 0};
+	}
+
+	// тут будем хранить все будущие ходы для их оценки
+	let moves = [];
+	// перебираем доступные клетки
+	for (let i = 0; i < availSpots.length; i++) {
+		// переменная для следующего шага
+		let move = {};
+		// делаем шаг в очередную пустую клетку и получаем новое положение на доске
+		move.index = newBoard[availSpots[i]];
+		// заполняем эту клетку символом того, чей ход мы рассчитываем
+		newBoard[availSpots[i]] = player;
+
+		// если считаем ход для компьютера
+		if (player == aiPlayer) {
+			// рекурсивно вызываем минимаксную функцию для нового положения и указываем, что следующий ход делает человек
+			let result = minimax(newBoard, huPlayer);
+			move.score = result.score;
+		// то же самое, но если считаем ход человека
+		} else {
+			let result = minimax(newBoard, aiPlayer);
+			move.score = result.score;
+		}
+		// запоминаем результат
+		newBoard[availSpots[i]] = move.index;
+		// добавляем ход в список ходов
+		moves.push(move);
+	}
+	// переменная для лучшего хода
+	let bestMove;
+	// если считаем ход компьютера
+	if(player === aiPlayer) {
+		// берём максимально низкое значение
+		var bestScore = -10000;
+		// перебираем все ходы, что у нас получились
+		for(let i = 0; i < moves.length; i++) {
+			// если очки текущего хода больше лучшего значения
+			if (moves[i].score > bestScore) {
+				// запоминаем это как лучшее значение
+				bestScore = moves[i].score;
+				// запоминаем номер хода
+				bestMove = i;
+			}
+		}
+	// то же самое делаем с ходом, если моделируем ход человека
+	} else {
+		let bestScore = 10000;
+		for(let i = 0; i < moves.length; i++) {
+			if (moves[i].score < bestScore) {
+				bestScore = moves[i].score;
+				bestMove = i;
+			}
+		}
+	}
+	// возвращаем лучший ход
+	return moves[bestMove];
+  }
+
+  bestSpot(squares)
+
   const winner = calculateWinner(squares);
   let status;
-  if (winner) {
-    status = 'Winner: ' + winner;
-  } else {
-    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
-  }
+  if (winner) status = 'Winner: ' + winner;
 
   return (
     <>
@@ -54,47 +124,27 @@ function Board({ xIsNext, squares, onPlay }) {
   );
 }
 
-export default function Game() {
-  const [history, setHistory] = useState([Array(9).fill(null)]);
-  const [currentMove, setCurrentMove] = useState(0);
-  const xIsNext = currentMove % 2 === 0;
-  const currentSquares = history[currentMove];
-
-  function handlePlay(nextSquares) {
-    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
-    setHistory(nextHistory);
-    setCurrentMove(nextHistory.length - 1);
-  }
-
-  function jumpTo(nextMove) {
-    setCurrentMove(nextMove);
-  }
-
-  const moves = history.map((squares, move) => {
-    let description;
-    if (move > 0) {
-      description = 'Go to move #' + move;
-    } else {
-      description = 'Go to game start';
-    }
-    return (
-      <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
-      </li>
-    );
-  });
-
-  return (
-    <div className="game">
-      <div className="game-board">
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
-      </div>
-      <div className="game-info">
-        <ol>{moves}</ol>
-      </div>
-    </div>
-  );
-}
+// function Cells () {
+//   return (
+//     <>
+//       <div className="board-row">
+//         <Square value={0} />
+//         <Square value={1} />
+//         <Square value={2} />
+//       </div>
+//       <div className="board-row">
+//         <Square value={3} />
+//         <Square value={4} />
+//         <Square value={5} />
+//       </div>
+//       <div className="board-row">
+//         <Square value={6} />
+//         <Square value={7} />
+//         <Square value={8} />
+//       </div>
+//     </>
+//   );
+// }
 
 function calculateWinner(squares) {
   const lines = [
@@ -107,6 +157,7 @@ function calculateWinner(squares) {
     [0, 4, 8],
     [2, 4, 6],
   ];
+
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
@@ -116,3 +167,24 @@ function calculateWinner(squares) {
   return null;
 }
 
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  // const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board squares={currentSquares} onPlay={handlePlay} />
+        {/* <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} /> */}
+      </div>
+    </div>
+  );
+}
